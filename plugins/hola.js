fe861113f9ab106c84c3a202e0138e093bea2075
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-03-09 19:11:59 -0500

    Add files via upload

--- plugins/hola.js
+++ plugins/hola.js
@@ -3,11 +3,11 @@ let path = require('path')
 
 let handler = async (m, { conn }) => {
 let vn = './media/holis.mp3'
-conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, {
+conn.sendFile(m.chat, vn, 'holis.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.command = /^(hola|ola|olis|Holis|Hola🤖|ola🤖)$/i
+handler.command = /^(hola|ola|Ola|olis|Holis|Hola🤖|ola🤖)$/i
 
 module.exports = handler
