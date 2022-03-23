629ae5b336178b16bbba1acc125774f79daafdca
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-03-23 17:43:32 -0500

    Add files via upload

--- plugins/pensamientos-sexuales.js
+++ plugins/pensamientos-sexuales.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Estas hot|Esta hot|Andan cogiendo|andan cogiendo|Estan cogiendo|estan cogiendo|Sueña conmigo|Están cogiendo|están cogiendo/
+handler.customPrefix = /Estas hot|Esta hot|Andan cogiendo|andan cogiendo|Estan cogiendo|estan cogiendo|Sueña conmigo|Están cogiendo|están cogiendo|Que ganas de coger|quiero coger/
 handler.command = new RegExp
 module.exports = handler
