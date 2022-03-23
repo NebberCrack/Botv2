fa2b8cde4d8b727306260c4b95aa1c077b3ece5f
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-03-23 18:44:35 -0500

    Add files via upload

--- plugins/guapo.js
+++ plugins/guapo.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Guapa|Guapo|guapa|guapo/
+handler.customPrefix = /Guapa|Guapo|guapa|guapo|linda|Linda|Relinda|Re linda|Hermoso|hermoso|Lindo|lindo/
 handler.command = new RegExp
 module.exports = handler
