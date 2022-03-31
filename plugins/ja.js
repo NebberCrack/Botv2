b556383677f15c9b7c07612a33c7e14b18178630
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-03-31 15:33:27 -0500

    Add files via upload

--- plugins/ja.js
+++ plugins/ja.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Jaja|jaja/
+handler.customPrefix = /Que risa|me causa gracia|que chistoso|que gracioso/
 handler.command = new RegExp
 module.exports = handler
