cc76d53db45669b388b907feb36011d02e56f0ef
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-04-01 00:03:31 -0500

    Add files via upload

--- plugins/no.js
+++ plugins/no.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /No quiero|no quiero|No chupalo|no chupalo/
+handler.customPrefix = /No quiero|no quiero|No chupalo|no chupalo|No chupala|no chupala/
 handler.command = new RegExp
 module.exports = handler
