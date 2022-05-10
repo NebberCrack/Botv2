dcc8d7eebc843bda4fdf426eb92d449f81dbfa77
NebberCrack <95258917+NebberCrack@users.noreply.github.com>
2022-05-10 11:09:34 -0500

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 function handler(m) {
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-this.sendContact(m.chat, '17722386341', 'Shadow - Creador - Oficial', m)}
+this.sendContact(m.chat, '994400781527', '𓊈𒆜ᴺᵉᵇᵇᵉʳᶜʳᵃᶜᵏᵉʳ𒆜𓊉ᴮᴼˢˢ', m)}
 handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 module.exports = handler
